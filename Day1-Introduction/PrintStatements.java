8e94a08a484fa27070fcfea74fd01bcc8384962a
Hiten-snapy <hitennalapatla@gmail.com>
2025-06-12 00:20:35 +0530

    Added PrintStatements.java to Day 1

---
+++ Day1-Introduction/PrintStatements.java
@@ -0,0 +1,7 @@
+public class PrintStatements {
+    public static void main(String[]args)
+    {
+        System.out.println("My name is Hiten");
+        System.out.println("Welcome to Java!");
+    }
+}
